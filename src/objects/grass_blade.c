0123204c6a7249708ca52abea41b23b986c058e0
Jay <jay.lecavalier@colorado.edu>
2015-06-12 18:45:18 -0600

    grass blade lookin' good

--- src/objects/grass_blade.c
+++ src/objects/grass_blade.c
@@ -10,17 +10,19 @@ void grass_blade() {
   glBegin(GL_QUAD_STRIP);
   // White, for now.
   glColor3f(1,1,1);
-  // Centeread around origin, then transform
   float i;
   float mpi6 = M_PI/6;
-  float mpi5 = M_PI/5;
-  for (i=(-M_PI/6);i <= mpi5;i+=0.001) {
+  float mpi3 = M_PI/3;
+  // First, we draw the base of the blade.
+  for (i=(-M_PI/6);i <= mpi3;i+=0.001) {
   	float cosi = cos(i);
-  	glVertex3f(cosi,i+mpi6,0);
-  	glVertex3f(1-cosi,i+mpi6,0);
-  	glVertex3f(cosi,i+.01+mpi6,0);
-  	glVertex3f(1-cosi,i+.01+mpi6,0);
+  	float z_pos = pow((mpi6+i)/1.5,2);
+  	glVertex3f(cosi,i+mpi6,z_pos);
+  	glVertex3f(1-cosi,i+mpi6,z_pos);
+  	glVertex3f(cosi,i+.01+mpi6,z_pos+.001);
+  	glVertex3f(1-cosi,i+.01+mpi6,z_pos+.001);
   }
   glEnd();
+  // Now, we draw the tip of the blade.
   glPopMatrix();
 }
